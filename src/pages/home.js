f89f53d29346e8c99b041d287ac0a920a76ecba3
RainanAraujo <rainanaraaujo@gmail.com>
2020-05-30 21:53:58 -0300

    Settings Screen ad TabBar Icons

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,15 +3,61 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import Profile from './profile';
 import Partnerships from './partnerships';
 import Settings from './settings';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 const Tab = createMaterialTopTabNavigator();
 
 export default function Home() {
   return (
     <>
-      <Tab.Navigator tabBarPosition="bottom">
-        <Tab.Screen name="Perfil" component={Profile} />
-        <Tab.Screen name="Parcerias" component={Partnerships} />
-        <Tab.Screen name="Definições" component={Settings} />
+      <Tab.Navigator
+        tabBarPosition="bottom"
+        tabBarOptions={{
+          showIcon: true,
+          showLabel: false,
+          indicatorStyle: {
+            backgroundColor: '#ffffff',
+          },
+        }}>
+        <Tab.Screen
+          name="Perfil"
+          component={Profile}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name="user-circle"
+                size={20}
+                color={focused ? '#141414' : '#A4A4A4'}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Parcerias"
+          component={Partnerships}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name="users"
+                size={20}
+                color={focused ? '#141414' : '#A4A4A4'}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Definições"
+          component={Settings}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name="cog"
+                size={20}
+                color={focused ? '#141414' : '#A4A4A4'}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </>
   );
