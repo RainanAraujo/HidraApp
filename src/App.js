5f66e333bc61b3e7458155c5963a7c3919aa7051
DeamonPedro <pedro.lucas.root@gmail.com>
2020-07-01 16:15:26 -0300

    added firebase notification module

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Routes from './routes';
 import messaging from '@react-native-firebase/messaging';
 
-messaging().setBackgroundMessageHandler(async remoteMessage => {
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   console.log('Message handled in the background!', remoteMessage);
 });
 
