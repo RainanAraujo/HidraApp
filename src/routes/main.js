b6b40487d62b58a0e0723d49fc5bf1760214faba
RainanAraujo <rainanaraaujo@gmail.com>
2020-07-03 20:31:42 -0300

    refactor aply uppercase in components paste

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../pages/login';
-import Home from './home';
+import Home from './Home';
 
 const AppStack = createStackNavigator();
 
