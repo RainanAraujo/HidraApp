fab177559c19277e9847cfc120e7eeb37ead176c
DeamonPedro <pedro.lucas.root@gmail.com>
2020-07-04 18:01:28 -0300

    refactor(services): rewritten service functions

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,56 +1,57 @@
 import auth from '@react-native-firebase/auth';
 
 export const Logout = () => {
-  auth().signOut();
+  return new Promise((resolve) => {
+    auth()
+      .signOut()
+      .then(() => {
+        resolve();
+      })
+      .catch(() => {
+        throw new Error('Erro ao realizar logout');
+      });
+  });
 };
 
 export const ChangePassword = (oldPassword, newPassword) => {
-  this.reauthenticate(oldPassword)
-    .then(() => {
-      var user = firebase.auth().currentUser;
-      user
-        .updatePassword(newPassword)
-        .then(() => {
-          return {status: 'success', msg: 'Senha Alterada'};
-        })
-        .catch((error) => {
-          return {status: 'error', msg: 'Erro ao Trocar Senha'};
-        });
-    })
-    .catch((error) => {
-      return {status: 'error', msg: 'Senha Incorreta'};
-    });
+  return new Promise((resolve) => {
+    this.reauthenticate(oldPassword)
+      .then(() => {
+        var user = firebase.auth().currentUser;
+        user
+          .updatePassword(newPassword)
+          .then(() => {
+            resolve();
+          })
+          .catch((error) => {
+            throw new Error('Erro ao Trocar Senha');
+          });
+      })
+      .catch((error) => {
+        throw new Error('Senha Incorreta');
+      });
+  });
 };
 
 export const Login = (email, pass) => {
-  if (email && pass) {
-    auth()
-      .signInWithEmailAndPassword(email.trim(), pass.trim())
-      .then((res) => {
-        return {
-          status: 'success',
-          msg: res.user.uid,
-        };
-      })
-      .catch((error) => {
-        if (error.code == 'auth/network-request-failed') {
-          return {status: 'error', msg: 'Erro na Rede'};
-        } else if (error.code == 'auth/invalid-email') {
-          return {
-            status: 'error',
-            msg: 'Email Inválido',
-          };
-        } else {
-          return {
-            status: 'error',
-            msg: 'Login Inválido',
-          };
-        }
-      });
-  } else {
-    return {
-      status: 'error',
-      msg: 'Login Inválido',
-    };
-  }
+  return new Promise((resolve) => {
+    if (email && pass) {
+      auth()
+        .signInWithEmailAndPassword(email.trim(), pass.trim())
+        .then((res) => {
+          resolve(res.user.uid);
+        })
+        .catch((error) => {
+          if (error.code == 'auth/network-request-failed') {
+            return {status: 'error', msg: 'Erro na Rede'};
+          } else if (error.code == 'auth/invalid-email') {
+            throw new Error('Email Inválido');
+          } else {
+            throw new Error('Login Inválido');
+          }
+        });
+    } else {
+      throw new Error('Login Inválido');
+    }
+  });
 };
