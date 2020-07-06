0433f5e28be029eff55aac4832b94be4a7711b90
RainanAraujo <rainanaraaujo@gmail.com>
2020-07-05 21:37:11 -0300

    refactor (contact) add contact modal

---
+++ src/components/Contact/index.js
@@ -0,0 +1,99 @@
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+  StatusBar,
+} from 'react-native';
+import styles from './styles';
+export default function Contact(props) {
+  const [currentContact, setCurrentContact] = useState(true);
+  return (
+    <Modal animationType="slide" visible={true} statusBarTranslucent={true}>
+      <StatusBar barStyle="dark-content" />
+
+      <View style={styles.modalContainer}>
+        {currentContact ? (
+          <>
+            <View>
+              <Text style={styles.textTitle}>
+                Observamos que já existe um contato cadastrado!
+              </Text>
+              <View style={styles.currentContact}>
+                <Text style={styles.textCurrentContact}>+55(99)9817-9097</Text>
+              </View>
+            </View>
+            <View style={styles.form}>
+              <Text style={styles.textSubTitle}>
+                Caso deseje alterá-lo insira no campo abaixo ultilizando somente
+                números.
+              </Text>
+              <View style={{alignItems: 'center'}}>
+                <TextInput
+                  style={styles.Input}
+                  placeholder="(DDD) 99999-9999"
+                  autoCompleteType="off"
+                  textContentType="telephoneNumber"
+                  keyboardType="number-pad"
+                />
+              </View>
+              <View style={styles.butons}>
+                <TouchableOpacity style={styles.butonAcept}>
+                  <Text style={styles.textButonAccept}>Adicionar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.butonCancel}
+                  onPress={() => {
+                    props.onClose();
+                  }}>
+                  <Text style={styles.textButonClose}>Cancelar</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View />
+          </>
+        ) : (
+          <>
+            <View>
+              <Text style={styles.textTitle}>Adicionar Contato</Text>
+              <Text style={styles.textSubTitle}>
+                Ao adicionar seu contato no perfil, ele será visível para os
+                demais membros da atlética.
+              </Text>
+            </View>
+
+            <View style={styles.form}>
+              <View style={{alignItems: 'center'}}>
+                <Text style={styles.textWarning}>
+                  * Preencha somente com números
+                </Text>
+                <TextInput
+                  style={styles.Input}
+                  placeholder="(DDD) 99999-9999"
+                  autoCompleteType="off"
+                  textContentType="telephoneNumber"
+                  keyboardType="number-pad"
+                />
+              </View>
+              <View style={styles.butons}>
+                <TouchableOpacity style={styles.butonAcept}>
+                  <Text style={styles.textButonAccept}>Adicionar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.butonCancel}
+                  onPress={() => {
+                    props.onClose();
+                  }}>
+                  <Text style={styles.textButonClose}>Cancelar</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View />
+          </>
+        )}
+      </View>
+    </Modal>
+  );
+}
--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -8,6 +8,7 @@ import {
   SafeAreaView,
 } from 'react-native';
 import ChangePasswordModal from '../../components/ChangePassword';
+import Contact from '../../components/Contact';
 import ToggleSwitch from 'toggle-switch-react-native';
 import Icon from 'react-native-vector-icons/dist/Feather';
 import {signOut} from '../../services/auth';
@@ -19,7 +20,7 @@ import {Assets} from '@react-navigation/stack';
 export default function Settings({navigation}) {
   const dispatch = useDispatch();
   const [modalPasswordVisible, setModalPasswordVisible] = useState(false);
-
+  const [modalContactVisible, setModalContactVisible] = useState(false);
   const [ToggleDarkMode, setToggleDarkMode] = useState(false);
   const [ToggleNotification, setToggleNotification] = useState(false);
   const [PasswordVisible, setPasswordVisible] = useState(false);
@@ -27,98 +28,113 @@ export default function Settings({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-      {modalPasswordVisible ? (
-        <ChangePasswordModal onClose={() => setModalPasswordVisible(false)} />
+      {}
+      {modalContactVisible ? (
+        <Contact onClose={() => setModalContactVisible(false)} />
       ) : (
         <>
-          <View style={styles.titleContent}>
-            <Text style={styles.textTitle}>Ajustes</Text>
-            <Text style={styles.textSubTitle}>Isso é auto-explicativo.</Text>
-          </View>
-          <TouchableOpacity
-            onPress={() => setModalPasswordVisible(true)}
-            style={styles.button}>
-            <View style={styles.iconAdjustWithName}>
-              <Icon name="lock" size={20} />
-              <Text style={styles.textButton}>Alterar Senha</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <View style={styles.iconAdjustWithName}>
-              <Icon name="user" size={20} />
-              <Text style={styles.textButton}>Alterar foto de perfil</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <View style={styles.iconAdjustWithName}>
-              <Icon name="phone" size={20} />
-              <Text style={styles.textButton}>Adicionar contato</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            disabled={true}
-            style={styles.button}
-            onPress={() => {
-              ToggleDarkMode
-                ? setToggleDarkMode(false)
-                : setToggleDarkMode(true);
-            }}>
+          {modalPasswordVisible ? (
+            <ChangePasswordModal
+              onClose={() => setModalPasswordVisible(false)}
+            />
+          ) : (
             <>
-              <View style={styles.iconAdjustWithName}>
-                <Icon name="moon" size={20} />
-                <Text style={styles.textButton}>Modo escuro (Em Breve)</Text>
+              <View style={styles.titleContent}>
+                <Text style={styles.textTitle}>Ajustes</Text>
+                <Text style={styles.textSubTitle}>
+                  Isso é auto-explicativo.
+                </Text>
               </View>
-              <View>
-                <ToggleSwitch
-                  disabled={true}
-                  isOn={ToggleDarkMode}
-                  onColor="#38B124"
-                  offColor="#ecf0f1"
-                  onToggle={() => {
-                    ToggleDarkMode
-                      ? setToggleDarkMode(false)
-                      : setToggleDarkMode(true);
-                  }}
-                />
-              </View>
-            </>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              ToggleNotification
-                ? setToggleNotification(false)
-                : setToggleNotification(true);
-            }}>
-            <>
-              <View style={styles.iconAdjustWithName}>
-                <Icon name="bell" size={20} />
-                <Text style={styles.textButton}>Notificações</Text>
-              </View>
-              <View>
-                <ToggleSwitch
-                  isOn={ToggleNotification}
-                  onColor="#38B124"
-                  offColor="#ecf0f1"
-                  onToggle={() => {
-                    ToggleNotification
-                      ? setToggleNotification(false)
-                      : setToggleNotification(true);
-                  }}
-                />
-              </View>
-            </>
-          </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => setModalPasswordVisible(true)}
+                style={styles.button}>
+                <View style={styles.iconAdjustWithName}>
+                  <Icon name="lock" size={20} />
+                  <Text style={styles.textButton}>Alterar Senha</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button}>
+                <View style={styles.iconAdjustWithName}>
+                  <Icon name="user" size={20} />
+                  <Text style={styles.textButton}>Alterar foto de perfil</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => setModalContactVisible(true)}>
+                <View style={styles.iconAdjustWithName}>
+                  <Icon name="phone" size={20} />
+                  <Text style={styles.textButton}>Adicionar contato</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                disabled={true}
+                style={styles.button}
+                onPress={() => {
+                  ToggleDarkMode
+                    ? setToggleDarkMode(false)
+                    : setToggleDarkMode(true);
+                }}>
+                <>
+                  <View style={styles.iconAdjustWithName}>
+                    <Icon name="moon" size={20} />
+                    <Text style={styles.textButton}>
+                      Modo escuro (Em Breve)
+                    </Text>
+                  </View>
+                  <View>
+                    <ToggleSwitch
+                      disabled={true}
+                      isOn={ToggleDarkMode}
+                      onColor="#38B124"
+                      offColor="#ecf0f1"
+                      onToggle={() => {
+                        ToggleDarkMode
+                          ? setToggleDarkMode(false)
+                          : setToggleDarkMode(true);
+                      }}
+                    />
+                  </View>
+                </>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  ToggleNotification
+                    ? setToggleNotification(false)
+                    : setToggleNotification(true);
+                }}>
+                <>
+                  <View style={styles.iconAdjustWithName}>
+                    <Icon name="bell" size={20} />
+                    <Text style={styles.textButton}>Notificações</Text>
+                  </View>
+                  <View>
+                    <ToggleSwitch
+                      isOn={ToggleNotification}
+                      onColor="#38B124"
+                      offColor="#ecf0f1"
+                      onToggle={() => {
+                        ToggleNotification
+                          ? setToggleNotification(false)
+                          : setToggleNotification(true);
+                      }}
+                    />
+                  </View>
+                </>
+              </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={async () => {
-              await signOut();
-              dispatch({type: 'SET_USER_DATA', data: {}});
-              navigation.replace('Login');
-            }}>
-            <Text style={styles.textExitButton}>Sair</Text>
-          </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={async () => {
+                  await signOut();
+                  dispatch({type: 'SET_USER_DATA', data: {}});
+                  navigation.replace('Login');
+                }}>
+                <Text style={styles.textExitButton}>Sair</Text>
+              </TouchableOpacity>
+            </>
+          )}
         </>
       )}
     </SafeAreaView>
